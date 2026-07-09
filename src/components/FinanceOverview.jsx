6cede516a0c04e058aa0828c832f1a399bd95691
Suborno Naskar <subornonaskar2441139@gmail.com>
2026-07-09 21:41:26 +0530

    innitial frontend commit

---
+++ src/components/FinanceOverview.jsx
@@ -0,0 +1,29 @@
+import CustomPieChart from "./CustomPieChart.jsx";
+import {addThousandsSeparator} from "../util/util.js";
+
+const FinanceOverview = ({totalBalance, totalIncome, totalExpense}) => {
+    const COLORS = ["#7620b7", "#c31016", "#05994a"];
+
+    const balanceData = [
+        { name: "Total Balance", amount: totalBalance },
+        { name: "Total Expenses", amount: totalExpense },
+        { name: "Total Income", amount: totalIncome },
+    ];
+    return (
+        <div className="card">
+            <div className="flex items-center justify-between">
+                <h5 className="text-lg">Financial Overview</h5>
+            </div>
+
+            <CustomPieChart
+                data={balanceData}
+                label="Total Balance"
+                totalAmount={`₹${addThousandsSeparator(totalBalance)}`}
+                colors={COLORS}
+                showTextAnchor
+            />
+        </div>
+    )
+}
+
+export default FinanceOverview;
