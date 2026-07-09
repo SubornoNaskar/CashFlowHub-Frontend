c9574cfcbce8664e4c946acf18f10b5c17ca8748
Suborno Naskar <subornonaskar2441139@gmail.com>
2026-07-10 01:39:58 +0530

    update color

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -43,7 +43,8 @@ const Menubar = ({ activeMenu }) => {
     };
 
     return (
-        <div className="flex items-center justify-between gap-5 bg-white border border-b border-gray-200/50 backdrop-blur-[2px] py-4 px-4 sm:px-7 sticky top-0 z-30">
+        <div className="flex items-center justify-between gap-5 bg-gradient-to-r from-blue-100 via-sky-50 to-white border-b 
+        border-blue-200 shadow-md py-4 px-6">
             {/* Left side - Menu button and title */}
             <div className="flex items-center gap-5">
                 <button
